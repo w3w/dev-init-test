da7f133923161e87962e5199ec7823d26446f441
Michal Vrchota <michal.vrchota@gmail.com>
2016-02-29 13:46:52 +0100

    Task 4 bughunt

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# dev-init-test
+Zadání
+======
+
+Podívej se do adresáře src/ a zkus najít chybu v kódu. 
+To že se ti kód nepodaří spustit není tvoje chyba :)
+
+* Pokud si myslíš, že si chybu objevil, napiš ji k docblocku dané metody. 
+* Případně zkus připravit opravu formou commitu
---
+++ src/ElasticaIndexer.php
@@ -0,0 +1,31 @@
+<?php
+
+class ElasticaIndexer
+{
+    const BULK_LIMIT = 100;
+
+    public function updateDocuments(Zend_Db_Select $select, \Elastica\Index $index)
+    {
+        $index->setSettings(array('index' => array('refresh_interval' => '-1')));
+        $stmt = $select->query(Zend_Db::FETCH_ASSOC);
+        $bulkData = [];
+        $numberOfDocumentsIndexed = 0;
+        while ($data = $stmt->fetch()) {
+            $id = (int) $data['id'];
+            unset($data['id']);
+            $document = new \Elastica\Document($id, $data);
+            $bulkData[] = $document;
+            
+            if (count($bulkData) >= self::BULK_LIMIT) {
+                $index->getType('products')->addDocuments($bulkData);
+                $numberOfDocumentsIndexed++;
+            }
+        }
+        $stmt->closeCursor();
+        $index->flush();
+        $index->refresh();
+        $index->optimize();
+        $index->setSettings(array('index' => array('refresh_interval' => '1s')));
+        return $numberOfDocumentsIndexed;
+    }
+}
